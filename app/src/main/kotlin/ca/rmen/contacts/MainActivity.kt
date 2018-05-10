ec4a8693c45380b627e4de42f0dec70c4dad956e
Carmen Alvarez <c@rmen.ca>
2018-05-10 13:31:38 +0200

    Request permissions

--- app/src/main/kotlin/ca/rmen/contacts/MainActivity.kt
+++ app/src/main/kotlin/ca/rmen/contacts/MainActivity.kt
@@ -16,25 +16,43 @@
 
 package ca.rmen.contacts
 
+import android.Manifest
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.arch.paging.PagedList
+import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import ca.rmen.contacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val REQUEST_CODE_PERMISSION = 1337
+    }
+
+    private lateinit var viewModel: ContactsViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ContactsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val viewModel = ViewModelProviders.of(this).get(ContactsViewModel::class.java)
-        val adapter = ContactsAdapter()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(ContactsViewModel::class.java)
+        adapter = ContactsAdapter()
         binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.recyclerView.adapter = adapter
-        val observer: Observer<PagedList<Contact>> = Observer { t -> adapter.submitList(t) }
-        viewModel.pagedList.observe(this, observer)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val observer: Observer<PagedList<Contact>> = Observer { t -> adapter.submitList(t) }
+            viewModel.pagedList.observe(this, observer)
+        }
     }
 }
